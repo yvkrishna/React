05805e60fa28216362a0ce239d6a269a213532cf
Vedha Krishna Yarasuri <vedhakrishnapanchami@gmail.com>
2020-03-21 15:32:17 +0530

    Full stack starts here

---
+++ fullweb/src/components/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () =>{
+    return(
+        <div className="container">
+            <h4>About</h4>
+            <p>Lorem Ipsum</p>
+        </div>
+    )
+}
+export default About
---
+++ fullweb/src/components/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () =>{
+    return(
+        <div className="container">
+            <h4>Contact</h4>
+            <p>Lorem Ipsum</p>
+        </div>
+    )
+}
+export default Contact
